71136cdcbfcfaa5999aed535772c4aa6fa6416f4
Anja Menkovic <anjamenkovic28@gmail.com>
2025-02-16 22:57:11 +0100

    Added authentication

--- studentski-kalendar/routes/api.php
+++ studentski-kalendar/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ActivityCategoryController;
 use App\Http\Controllers\ActivityController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CalendarController;
 use App\Http\Controllers\CalendarViewController;
 use App\Http\Controllers\NotificationController;
@@ -13,49 +14,55 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+// Rute za autentikaciju
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
 
-// Automatski generisane RESTful rute
-Route::apiResource('activities', ActivityController::class);
-Route::apiResource('activity-categories', ActivityCategoryController::class);
-Route::apiResource('calendars', CalendarController::class);
-Route::apiResource('calendar-views', CalendarViewController::class);
-Route::apiResource('notifications', NotificationController::class);
-Route::apiResource('users', UserController::class);
+    // Automatski generisane RESTful rute
+    Route::apiResource('activities', ActivityController::class);
+    Route::apiResource('activity-categories', ActivityCategoryController::class);
+    Route::apiResource('calendars', CalendarController::class);
+    Route::apiResource('calendar-views', CalendarViewController::class);
+    Route::apiResource('notifications', NotificationController::class);
+    Route::apiResource('users', UserController::class);
 
-// Ruta za testiranje
-Route::get('/greeting', function () {
-    return 'Hello World';
-});
+    // Ruta za testiranje
+    Route::get('/greeting', function () {
+        return 'Hello World';
+    });
 
-// Rute za studente
-Route::group(['middleware' => ['auth:api', 'role:student']], function () {
-    Route::get('activities', [ActivityController::class, 'index']);
-    Route::get('activities/{id}', [ActivityController::class, 'show']);
-    Route::post('activities', [ActivityController::class, 'store']);
-    Route::put('activities/{id}', [ActivityController::class, 'update']);
-    Route::delete('activities/{id}', [ActivityController::class, 'destroy']);
-    Route::get('calendars', [CalendarController::class, 'index']);
-    Route::get('calendars/{id}', [CalendarController::class, 'show']);
-    Route::get('notifications', [NotificationController::class, 'index']);
-    Route::get('notifications/{id}', [NotificationController::class, 'show']);
-});
+    // Rute za studente
+    Route::group(['middleware' => ['role:student']], function () {
+        Route::get('activities', [ActivityController::class, 'index']);
+        Route::get('activities/{id}', [ActivityController::class, 'show']);
+        Route::post('activities', [ActivityController::class, 'store']);
+        Route::put('activities/{id}', [ActivityController::class, 'update']);
+        Route::delete('activities/{id}', [ActivityController::class, 'destroy']);
+        Route::get('calendars', [CalendarController::class, 'index']);
+        Route::get('calendars/{id}', [CalendarController::class, 'show']);
+        Route::get('notifications', [NotificationController::class, 'index']);
+        Route::get('notifications/{id}', [NotificationController::class, 'show']);
+    });
 
-// Rute za administratore
-Route::group(['middleware' => ['auth:api', 'role:admin']], function () {
-    Route::post('activities', [ActivityController::class, 'store']);
-    Route::put('activities/{id}', [ActivityController::class, 'update']);
-    Route::delete('activities/{id}', [ActivityController::class, 'destroy']);
-    Route::post('activity-categories', [ActivityCategoryController::class, 'store']);
-    Route::put('activity-categories/{id}', [ActivityCategoryController::class, 'update']);
-    Route::delete('activity-categories/{id}', [ActivityCategoryController::class, 'destroy']);
-    Route::post('calendars', [CalendarController::class, 'store']);
-    Route::put('calendars/{id}', [CalendarController::class, 'update']);
-    Route::delete('calendars/{id}', [CalendarController::class, 'destroy']);
-    Route::post('notifications', [NotificationController::class, 'store']);
-    Route::put('notifications/{id}', [NotificationController::class, 'update']);
-    Route::delete('notifications/{id}', [NotificationController::class, 'destroy']);
-    Route::post('users', [UserController::class, 'store']);
-    Route::put('users/{id}', [UserController::class, 'update']);
-    Route::delete('users/{id}', [UserController::class, 'destroy']);
+    // Rute za administratore
+    Route::group(['middleware' => ['role:admin']], function () {
+        Route::post('activities', [ActivityController::class, 'store']);
+        Route::put('activities/{id}', [ActivityController::class, 'update']);
+        Route::delete('activities/{id}', [ActivityController::class, 'destroy']);
+        Route::post('activity-categories', [ActivityCategoryController::class, 'store']);
+        Route::put('activity-categories/{id}', [ActivityCategoryController::class, 'update']);
+        Route::delete('activity-categories/{id}', [ActivityCategoryController::class, 'destroy']);
+        Route::post('calendars', [CalendarController::class, 'store']);
+        Route::put('calendars/{id}', [CalendarController::class, 'update']);
+        Route::delete('calendars/{id}', [CalendarController::class, 'destroy']);
+        Route::post('notifications', [NotificationController::class, 'store']);
+        Route::put('notifications/{id}', [NotificationController::class, 'update']);
+        Route::delete('notifications/{id}', [NotificationController::class, 'destroy']);
+        Route::post('users', [UserController::class, 'store']);
+        Route::put('users/{id}', [UserController::class, 'update']);
+        Route::delete('users/{id}', [UserController::class, 'destroy']);
+    });
 });
